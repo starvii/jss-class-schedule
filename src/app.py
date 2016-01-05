89e79b06c7977d6881d4c267374cc84713d5a6c1
starvii <starvii@gmail.com>
2016-01-05 12:37:10 +0800

    代码库合并

---
+++ src/app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf8 -*-
+'''
+Created on 2016骞�1鏈�5鏃�
+
+@author: starvii
+'''
+
+def printhello():
+    print('helloWorld!')
+
+if __name__ == '__main__':
+    printhello()
---
+++ src/util/dateutil.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+Created on 2016年1月5日
+
+@author: starvii
+'''
+
+import sys
+sys.path.insert(0, '..')
+import app
+
+if __name__ == '__main__':
+    app.printhello()
